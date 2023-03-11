2512e62085660a47523c109f00ed01081237e6dd
Артём <av1965k@yandex.ru>
2023-03-11 23:08:52 +0400

    Попытка 1

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,11 +2,12 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
+    group = SlugRelatedField(read_only=True, slug_field='title')
 
     class Meta:
         fields = '__all__'
@@ -14,10 +15,18 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
+    author = SlugRelatedField(
         read_only=True, slug_field='username'
     )
 
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post', 'author')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,22 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+
+from .views import CommentViewSet, GroupViewSet, PostViewSet
+
+ENDPOITS = [
+    (r'posts', PostViewSet, 'posts'),
+    (r'posts/(?P<post_id>[1-9]\d*)/comments', CommentViewSet, 'comments'),
+    (r'groups', GroupViewSet, 'groups'),
+]
+
+router = DefaultRouter()
+
+for endpoint, viewset, basename in ENDPOITS:
+    router.register(endpoint, viewset, basename=basename)
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
